c12a05ae4cfde27543c8a82e1685ed7590d4cb45
AssemWaleed <117918691+AssemWaleed@users.noreply.github.com>
2024-03-06 13:37:23 +0200

    Update loginSystem.js

--- functions/loginSystem.js
+++ functions/loginSystem.js
@@ -8,6 +8,36 @@ const numbers = [
 var tries = [];
 
 
+
+
+
+
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+
+  function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("passwordInput");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
+
+
+
+
 $(document).ready(() => {
    
 
@@ -53,43 +83,224 @@ function loggedIn() {
 
 function pickVideo(n) { // Samir Yasser 5547
 
-    if (n == 0) {
-      
-        $(".video").attr("src", "https://go.screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
-    
-       
-        $("h1[title]").text("Senior 1 Math Trignometric Identities").css("text-align", "left");
+        if (n == 0) {
 
-    }
-    
+            alert("تنبيه لديك مشاهدتين فقط ")
+
+
+            $(".video").attr("src", "https://go.screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+            $(".video").attr("controlsList", "nodownload");
+            $("h1[title]").text("Senior 1 Math Trignometric Identities").css("text-align", "left");
+
+
+            var count = localStorage.getItem('viewCount0');
+
+            if (count === null) {
+                count = 0;
+            }
+
+          
+
+            
+            
+            
+            var watchDiv = document.querySelector('.watch');
+
+            var h3Element = document.createElement('h3');
+
+           
+            h3Element.textContent = 'مشاهدات : ' + count;
+            h3Element.style.textAlign = 'right';
+
+            document.body.appendChild(h3Element);
+
+            watchDiv.appendChild(h3Element);
+
+            count++;
+
+           
+            h3Element.textContent = 'مشاهدات : ' + count;
+
+            if (count >= 3) {
+                
+                document.getElementById('error').play();
+                alert("لقد انتهت المشاهدات عندك")
+                h3Element.textContent = 'تم انتهاء عدد المشاهدات';
+
+            }
+
+
+            localStorage.setItem('viewCount0', count);
+
+           
+
+            if (count >= 3) {
+                
+                
+                $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+            }
+            
+        }
+   
+        
+
+   
     if (n == 1) {  // Shahd Mansour 1973
 
+        alert("تنبيه لديك مشاهدتين فقط ")
+
        
-        $(".video").attr("src", "https://go.screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
-    
-       
+        $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+        $(".video").attr("controlsList", "nodownload");
         $("h1[title]").text("Senior 1 Math Trignometric Identities").css("text-align", "left");
 
+
+        var count1 = localStorage.getItem('viewCount1');
+
+        if (count1 === null) {
+            count1 = 0;
+        }
+
+
+        
+        
+        var watchDiv = document.querySelector('.watch');
+
+        var h3Element1 = document.createElement('h3');
+
+       
+        h3Element1.textContent = 'مشاهدات : ' + count1;
+        h3Element1.style.textAlign = 'right';
+
+        document.body.appendChild(h3Element1);
+
+        watchDiv.appendChild(h3Element1);
+
+        count1++;
+
+       
+        h3Element1.textContent = 'مشاهدات : ' + count1;
+
+        if (count1 >= 3) {
+            document.getElementById('error').play();
+            alert("لقد انتهت المشاهدات عندك")
+            h3Element1.textContent = 'تم انتهاء عدد المشاهدات';
+
+        }
+
+
+        localStorage.setItem('viewCount1', count1);
+
+       
+
+        if (count1 >= 3) {
+            
+            
+            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+        }
+
+        
+        
+
     }
+    
     if (n == 2) {   //  Fares waleed 4457
 
-        $(".video").attr("src", "https://go.screenpal.com/player/cZeerxVKBYF?width=100%&height=100%&ff=1&title=0");
+        alert("تنبيه لديك مشاهدتين فقط ")
+
+        $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
     
-       
+        $(".video").attr("controlsList", "nodownload");
         $("h1[title]").text("Senior 2 Calculus Rate Of Change").css("text-align", "left");;
+
+        var count2 = localStorage.getItem('viewCount2');
+
+        if (count2 === null) {
+            count2 = 0;
+        }
+
+        
+        
+        
+        
+        var watchDiv = document.querySelector('.watch');
+
+        var h3Element2 = document.createElement('h3');
+
+       
+        h3Element2.textContent = 'مشاهدات : ' + count2;
+        h3Element2.style.textAlign = 'right';
+
+        document.body.appendChild(h3Element2);
+
+        watchDiv.appendChild(h3Element2);
+
+        count2++;
+
+       
+        h3Element2.textContent = 'مشاهدات : ' + count2;
+
+        if (count2 >= 3) {
+            document.getElementById('error').play();
+            alert("لقد انتهت المشاهدات عندك")
+            h3Element2.textContent = 'تم انتهاء عدد المشاهدات';
+
+        }
+
+
+        localStorage.setItem('viewCount2', count2);
+
+       
+
+        if (count2 >= 3) {
+            
+            
+            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+        }
+        
+
+       
+        
+        
     }
 
  
     
     if (n === 3) {   // Aya Medhat 5674
-
-    
-      
-        $(".video").attr("src", "https://go.screenpal.com/player/cZeerxVKBYF?width=100%&height=100%&ff=1&title=0");
-    
-       
-        $("h1[title]").text("Senior 2 Calculus Rate Of Change").css("text-align", "left");;
+        alert("تنبيه لديك مشاهدتين فقط ");
         
+        $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+        $(".video").attr("controlsList", "nodownload");
+        $("h1[title]").text("Senior 2 Calculus Rate Of Change").css("text-align", "left");
+    
+        var count3 = parseInt(localStorage.getItem('viewCount3'));
+        if (isNaN(count3)) { // Check if count3 is NaN
+            count3 = 0;
+        }
+    
+        var watchDiv = document.querySelector('.watch');
+        var h3Element3 = document.createElement('h3');
+    
+        h3Element3.textContent = 'مشاهدات : ' + count3;
+        h3Element3.style.textAlign = 'right';
+    
+        watchDiv.appendChild(h3Element3);
+    
+        count3++;
+    
+        h3Element3.textContent = 'مشاهدات : ' + count3;
+    
+        if (count3 >= 3) {
+            document.getElementById('error').play();
+            alert("لقد انتهت المشاهدات عندك");
+            h3Element3.textContent = 'تم انتهاء عدد المشاهدات';
+        }
+    
+        localStorage.setItem('viewCount3', count3);
+    
+        if (count3 >= 3) {
+            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+        }
     }
     
         
