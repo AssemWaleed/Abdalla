8316f5aae893067584d350b7850180206ef8e3a2
AssemWaleed <117918691+AssemWaleed@users.noreply.github.com>
2024-03-09 23:59:38 +0200

    Update loginSystem.js

--- functions/loginSystem.js
+++ functions/loginSystem.js
@@ -218,7 +218,7 @@ function pickVideo(n) { // Samir Yasser 5547
 
 
         localStorage.setItem('viewCount1', count1);
-        localStorage.setItem('viewCount1', 0);
+        
         
         
 
