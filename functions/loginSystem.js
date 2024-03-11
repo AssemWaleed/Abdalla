2eadf4f5b5ea192857765aeff7d46bb7e7466ec3
AssemWaleed <117918691+AssemWaleed@users.noreply.github.com>
2024-03-11 14:29:43 +0200

    Update loginSystem.js

--- functions/loginSystem.js
+++ functions/loginSystem.js
@@ -2,7 +2,8 @@ const numbers = [
     ["5547"],  // Samir Yasser
     ["1973"],  // Shahd Mansour
     ["4457"],  // Fares waleed
-    ["5674"]   // Aya Medhat 
+    ["5674"],   // Aya Medhat 
+    ["7893"]  // Farah Hassan
 ];
 
 var tries = [];
@@ -82,13 +83,14 @@ function loggedIn() {
 }
 
 function pickVideo(n) { // Samir Yasser 5547
+        
 
         if (n == 0) {
 
             
 
 
-            $(".video").attr("src", "https://go.screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+            $(".video").attr("src", "https://go.screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0")
             $(".video").attr("controlsList", "nodownload");
             $("h1[title]").text("Senior 1 Math Trignometric Identities").css("text-align", "left");
 
@@ -98,9 +100,9 @@ function pickVideo(n) { // Samir Yasser 5547
             if (count === null) {
                 count = 0;
             }
-    
+
+
             
-           
            
             
             
@@ -145,7 +147,9 @@ function pickVideo(n) { // Samir Yasser 5547
            
 
             localStorage.setItem('viewCount0', count);
+            localStorage.setItem('viewCount0', 0);
             
+           
 
            
             
@@ -160,14 +164,44 @@ function pickVideo(n) { // Samir Yasser 5547
             
         }
    
-        
+        $("button#myButton2").click(() => {  // فديو الواجب
+            const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://www.youtube.com/embed/Kkp8-qHWWno?start=1370");
+                $("#myButton2").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton3").css("color", "black");
+                $("#myButton4").css("color", "black");
+            } else {
+                none;
+            }
+           
+        });
+
+
+
+        $("button#myButton4").click(() => {  // الامتحان
+            //const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+                $("#myButton4").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton2").css("color", "black");
+                $("#myButton3").css("color", "black");
+            } else {
+                none;
+            }
+       
+        });
 
    
     if (n == 1) {  // Shahd Mansour 1973
 
 
        
-        $(".video").attr("src", "https://go.screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+        $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560")
         $(".video").attr("controlsList", "nodownload");
         $("h1[title]").text("Senior 1 Math Trignometric Identities").css("text-align", "left");
 
@@ -216,19 +250,53 @@ function pickVideo(n) { // Samir Yasser 5547
 
         }
 
-
+        
         localStorage.setItem('viewCount1', count1);
         
         
         
-
+        
        
 
         if (count1 >= 3) {
             
             
-            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560").addEventListener('contextmenu', function(e) {
+                e.preventDefault();
+            });;
         }
+
+        $("button#myButton2").click(() => {  // فديو الواجب
+            const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://www.youtube.com/embed/Kkp8-qHWWno?start=1370");
+                $("#myButton2").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton3").css("color", "black");
+                $("#myButton4").css("color", "black");
+            } else {
+                none;
+            }
+           
+        });
+
+
+
+        $("button#myButton4").click(() => {  // الامتحان
+            const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+                $("#myButton4").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton2").css("color", "black");
+                $("#myButton3").css("color", "black");
+            } else {
+                none;
+            }
+       
+        });
 
         
         
@@ -239,7 +307,7 @@ function pickVideo(n) { // Samir Yasser 5547
 
         
 
-        $(".video").attr("src", "https://go.screenpal.com/player/cZeerxVKBYF?width=100%&height=100%&ff=1&title=0");
+        $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
     
         $(".video").attr("controlsList", "nodownload");
         $("h1[title]").text("Senior 2 Calculus Rate Of Change").css("text-align", "left");;
@@ -292,19 +360,49 @@ function pickVideo(n) { // Samir Yasser 5547
 
         localStorage.setItem('viewCount2', count2);
         
-        
-        
 
        
 
         if (count2 >= 3) {
             
             
-            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560").addEventListener('contextmenu', function(e) {
+                e.preventDefault();
+            });;
         }
         
 
+        $("button#myButton2").click(() => {  // فديو الواجب
+            const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://www.youtube.com/embed/Kkp8-qHWWno?start=1370");
+                $("#myButton2").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton3").css("color", "black");
+                $("#myButton4").css("color", "black");
+            } else {
+                none;
+            }
+           
+        });
+
+
+
+        $("button#myButton4").click(() => {  // الامتحان
+            const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+                $("#myButton4").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton2").css("color", "black");
+                $("#myButton3").css("color", "black");
+            } else {
+                none;
+            }
        
+        });
         
         
     }
@@ -314,7 +412,7 @@ function pickVideo(n) { // Samir Yasser 5547
     if (n === 3) {   // Aya Medhat 5674
        
         
-        $(".video").attr("src", "https://go.screenpal.com/player/cZeerxVKBYF?width=100%&height=100%&ff=1&title=0");
+        $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
         $(".video").attr("controlsList", "nodownload");
         $("h1[title]").text("Senior 2 Calculus Rate Of Change").css("text-align", "left");
     
@@ -358,21 +456,141 @@ function pickVideo(n) { // Samir Yasser 5547
         }
     
         localStorage.setItem('viewCount3', count3);
-        
-        
-        
-        
-        
-        
-       
-        
        
 
     
         if (count3 >= 3) {
-            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560");
+            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560").addEventListener('contextmenu', function(e) {
+                e.preventDefault();
+            });;
         }
+
+        $("button#myButton2").click(() => {  // فديو الواجب
+            const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://www.youtube.com/embed/Kkp8-qHWWno?start=1370");
+                $("#myButton2").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton3").css("color", "black");
+                $("#myButton4").css("color", "black");
+            } else {
+                none;
+            }
+           
+        });
+
+
+
+        $("button#myButton4").click(() => {  // الامتحان
+            //const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+                $("#myButton4").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton2").css("color", "black");
+                $("#myButton3").css("color", "black");
+            } else {
+                none;
+            }
+       
+        });
+
     }
-    
+
+        if (n === 4) {   // Farah Hassan 7893
+        
+            
+            $(".video").attr("src", "https://go.screenpal.com/player/cZei3oVKNhc?width=100%&height=100%&ff=1&title=0");
+            $(".video").attr("controlsList", "nodownload");
+            $("h1[title]").text("Senior 2  Rectilinear motion with uniform acceleration").css("text-align", "left");
+        
+            var count4 = localStorage.getItem('viewCount4');
+
+            if (count4 === null) {
+                count4 = 0;
+            }
+            
+
+            
+
+
+
+            var watchDiv = document.querySelector('.watch');
+            var h3Element4 = document.createElement('h3');
+        
+            h3Element4.textContent = 'مشاهدات : ' + count4;
+            h3Element4.style.textAlign = 'right';
+        
+            watchDiv.appendChild(h3Element4);
+        
+            count4++;
+        
+            h3Element4.textContent = 'مشاهدات : ' + count4;
+
+            if (count4 === 1) {
+                alert("تنبيه: لديك مشاهدتين فقط ");
+
+            } else if (count4 === 2) {
+                alert("تنبيه: لديك مشاهدة واحدة فقط");
+
+            }
+            
+            
+        
+            if (count4 >= 3) {
+                document.getElementById('error').play();
+                alert("لقد انتهت المشاهدات عندك");
+                h3Element4.textContent = 'تم انتهاء عدد المشاهدات';
+            }
+        
+            localStorage.setItem('viewCount4', count4);
+            localStorage.setItem('viewCount4', 0);
+        
+
+        
+            if (count4 >= 3) {
+                $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560").addEventListener('contextmenu', function(e) {
+                    e.preventDefault();
+                });;
+            }
+
+            $("button#myButton2").click(() => {  // فديو الواجب
+                const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+                
+                if (result) {
+                    $(".video").attr("src", "https://www.youtube.com/watch?v=iiWMFn_gBiA");
+                    $("#myButton2").css("color", "#15920a");
+                    $("#myButton").css("color", "black");
+                    $("#myButton3").css("color", "black");
+                    $("#myButton4").css("color", "black");
+                } else {
+                    return; // or perform some other action here
+                }
+            });
+            
+            
+            $("button#myButton4").click(() => {  // الامتحان
+                const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+            
+                if (result) {
+                    $(".video").attr("src", "");
+                    $("#myButton4").css("color", "#15920a");
+                    $("#myButton").css("color", "black");
+                    $("#myButton2").css("color", "black");
+                    $("#myButton3").css("color", "black");
+                } else {
+                    return; // or perform some other action here
+                }
+            });
+            
+
+        }
+
+
+
+        
+        buttonContainers[0].appendChild(toggleButton);
         
 }
