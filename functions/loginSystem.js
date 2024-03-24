b6d65b46ede9ccd4935c2e795080cd253b994a2a
AssemWaleed <117918691+AssemWaleed@users.noreply.github.com>
2024-03-24 23:28:40 +0200

    Update loginSystem.js

--- functions/loginSystem.js
+++ functions/loginSystem.js
@@ -10,6 +10,7 @@ const numbers = [
     ["1150"], // remas
     ["2252"],   // Ahmed reda
     ["2314"],   // Fareda hethm 
+    ["2771"],   // Roaia Elsafi 2771
 
 ];
 
@@ -1252,7 +1253,7 @@ function pickVideo(n) { // Samir Yasser 5547
         localStorage.setItem('viewCount10', count10);
         
         
-        if (count9 >= 3) {
+        if (count10 >= 3) {
             $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560").addEventListener('contextmenu', function(e) {
                 e.preventDefault();
             });;
@@ -1310,6 +1311,114 @@ function pickVideo(n) { // Samir Yasser 5547
        
         });
     }
+
+
+    if (n === 11) {   // Roaia Elsafi 2771
+
+        var imageSrc = 'images/profile.png';
+        var altText = 'avatar';
+        var tooltipText = 'Name is Roaia , Id is <span style="color:red; cursor:pointer;" >2771</span>';
+        var tooltipTextID = '2771';
+        addProfileImageAndTooltip(imageSrc, altText, tooltipText, tooltipTextID);
+       
+        
+        $(".video").attr("src", "https://go.screenpal.com/player/cZeIcLV7iGq?width=100%&height=100%&ff=1&title=0");
+        $(".video").attr("controlsList", "nodownload");
+        $("h1[title]").text("Senior 2 Calculas Differentiation").css("text-align", "left");
+    
+        var count11 = localStorage.getItem('viewCount11');
+
+        if (count11 === null) {
+            count11 = 0;
+        }
+        
+
+
+
+        var watchDiv = document.querySelector('.watch');
+        var h3Element11 = document.createElement('h3');
+        
+        h3Element11.textContent = 'مشاهدات : ' + count11;
+        h3Element11.style.textAlign = 'right';
+        
+        var buttonContainer = document.getElementById('buttonContainer');
+        buttonContainer.appendChild(h3Element11);
+        
+        count11++;
+        
+        h3Element11.textContent = 'مشاهدات : ' + count11;
+        
+        if (count11 === 1) {
+            alert("تنبيه: لديك مشاهدتين فقط ");
+        
+        } else if (count11 === 2) {
+            alert("تنبيه: لديك مشاهدة واحدة فقط");
+        
+        }
+        
+        if (count11 >= 3) {
+            document.getElementById('error').play();
+            alert("لقد انتهت المشاهدات عندك");
+            h3Element9.textContent = 'تم انتهاء عدد المشاهدات';
+        }
+        
+        localStorage.setItem('viewCount11', count11);
+        
+        
+        if (count11 >= 3) {
+            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560").addEventListener('contextmenu', function(e) {
+                e.preventDefault();
+            });;
+        }
+        
+        var button = document.createElement("button");
+        
+        button.innerText = "فديو الواجب";
+        button.style.fontSize = '30px';
+        button.style.paddingTop = '9px';
+        
+
+
+        
+        
+        button.addEventListener("click", function() {  // الواجب
+
+            const re = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+        
+            if (re) {
+                $(".video").attr("src", "https://www.youtube.com/embed/ZSxXoTyPXkk");
+                button.style.color = '#15920a';
+            } else {
+                none;
+            }
+
+        });
+        
+        
+        var container = document.getElementById("watcher");
+        container.appendChild(button);
+        
+        
+
+        
+
+
+        $("button#myButton4").click(() => {  // الامتحان
+            //const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+                $("#myButton4").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton2").css("color", "black");
+                $("#myButton3").css("color", "black");
+            } else {
+                none;
+            }
+       
+        });
+    }
+   
    
 
 
