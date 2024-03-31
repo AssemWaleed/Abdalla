3bf06b0c49e4fba32a64837f8d7f54a4c0de1ba5
AssemWaleed <117918691+AssemWaleed@users.noreply.github.com>
2024-03-31 19:10:31 +0200

    Update loginSystem.js

--- functions/loginSystem.js
+++ functions/loginSystem.js
@@ -16,6 +16,7 @@ const numbers = [
     ["4567"], // Gana Atef 
     ["5535"], // Mariem Mohamed
     ["5321"], // Kareem
+    ["9321"], // Judy
 ];
 
 var tries = [];
@@ -1875,7 +1876,7 @@ function pickVideo(n) { // Samir Yasser 5547
    
     if (n === 16) { // Kareem 5321
 
-        var imageSrc = 'images/profile.png';
+        var imageSrc = 'images/profileMan.png';
         var altText = 'avatar';
         var tooltipText = 'Name is  Mariem Mohamed , Id is <span style="color:red; cursor:pointer;" >5535</span>';
         var tooltipTextID = '5535';
@@ -1977,6 +1978,109 @@ function pickVideo(n) { // Samir Yasser 5547
     }
    
 
+    if (n === 17) { // Judy 9321
+
+        var imageSrc = 'images/profile.png';
+        var altText = 'avatar';
+        var tooltipText = 'Name is  Judy , Id is <span style="color:red; cursor:pointer;" >9321</span>';
+        var tooltipTextID = '9321';
+        addProfileImageAndTooltip(imageSrc, altText, tooltipText, tooltipTextID);
+       
+        
+        $(".video").attr("src", "https://iframe.mediadelivery.net/embed/224107/761689b2-a7f4-4aa0-a933-f2bf46829f63?autoplay=false&loop=false&muted=false&preload=true&responsive=true");
+        $("h1[title]").text("Senior 2 Problems Lesson 1").css("text-align", "left");
+    
+        var count17 = localStorage.getItem('viewCount17');
+
+        if (count17 === null) {
+            count17 = 0;
+        }
+        
+
+
+
+        var watchDiv = document.querySelector('.watch');
+        var h3Element17 = document.createElement('h3');
+        
+        h3Element17.textContent = 'مشاهدات : ' + count17;
+        h3Element17.style.textAlign = 'right';
+        
+        var buttonContainer = document.getElementById('buttonContainer');
+        buttonContainer.appendChild(h3Element17);
+        
+        count17++;
+        
+        h3Element17.textContent = 'مشاهدات : ' + count17;
+        
+        if (count17 === 1) {
+            alert("تنبيه: لديك مشاهدتين فقط ");
+        
+        } else if (count17 === 2) {
+            alert("تنبيه: لديك مشاهدة واحدة فقط");
+        
+        }
+        
+        if (count17 >= 3) {
+            document.getElementById('error').play();
+            alert("لقد انتهت المشاهدات عندك");
+            h3Element16.textContent = 'تم انتهاء عدد المشاهدات';
+        }
+        
+        localStorage.setItem('viewCount17', count17);
+        
+        
+        
+        
+        if (count17 >= 3) {
+            $(".video").attr("src", "https://media.discordapp.net/attachments/1142201401540345986/1214812434242736158/Untitled-1.png?ex=65fa793e&is=65e8043e&hm=cdff451e5cd694d0dc6b0dc3d0e38c736df4e01dc7873656c234372aeebef0ac&=&format=webp&quality=lossless&width=980&height=560").addEventListener('contextmenu', function(e) {
+                e.preventDefault();
+            });;
+        }
+        
+       // var button = document.createElement("button");
+        
+        button.innerText = "فديو الواجب";
+        button.style.fontSize = '30px';
+        button.style.paddingTop = '9px';
+        
+        button.addEventListener("click", function() {  // الواجب
+
+            const re = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+        
+            if (re) {
+                $(".video").attr("src", "https://www.youtube.com/embed/FXyiZry3Ukw");
+                button.style.color = '#15920a';
+            } else {
+                none;
+            }
+
+        });
+        
+        
+        var container = document.getElementById("watcher");
+        container.appendChild(button);
+        
+        
+
+        
+
+
+        $("button#myButton4").click(() => {  // الامتحان
+            //const result = window.confirm("ستقوم بفقدان المشاهده الخاصه بك");
+
+            if (result) {
+                $(".video").attr("src", "https://screenpal.com/player/cZee3aVKBOm?width=100%&height=100%&ff=1&title=0");
+                $("#myButton4").css("color", "#15920a");
+                $("#myButton").css("color", "black");
+                $("#myButton2").css("color", "black");
+                $("#myButton3").css("color", "black");
+            } else {
+                none;
+            }
+       
+        });
+    }
+   
    
 
 
