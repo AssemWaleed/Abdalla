ca94193eec9d98b99c98fb7032f877c128e06fc0
AssemWaleed <117918691+AssemWaleed@users.noreply.github.com>
2024-05-06 19:41:15 +0300

    Update loginSystem.js

--- functions/loginSystem.js
+++ functions/loginSystem.js
@@ -511,8 +511,8 @@ function pickVideo(n) { // Samir Yasser 5547
         addProfileImageAndTooltip(imageSrc, altText, tooltipText, tooltipTextID);
        
         
-        $(".video").attr("src", "https://go.screenpal.com/player/cZf3YrVMQ4r?width=100%&height=100%&ff=1&title=0");
-        $("h1[title]").text("Senior 2 Sum Or Difference Of Angles").css("text-align", "left");
+        $(".video").attr("src", "https://go.screenpal.com/player/cZfuFZVMtT8?width=100%&height=100%&ff=1&title=0");
+        $("h1[title]").text("Senior 2 Session 2 Revision Mechanics").css("text-align", "left");
     
         var count3 = localStorage.getItem('viewCount3');
 
